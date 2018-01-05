e1246d4e6e0316724293ed61435c9990be23eb17
SilentDepth <silentdepth@outlook.com>
2018-01-06 01:24:19 +0800

    Header is almost done

--- src/app.vue
+++ src/app.vue
@@ -4,8 +4,6 @@
   import Rank from './components/rank.vue'
   import './style.css'
 
-  const RANKS_URL = '/stats-data/ranks/latest.json'
-
   export default {
     name: 'App',
     components: {
@@ -24,7 +22,7 @@
     },
     methods: {
       async getRanksJson() {
-        const {data: json} = await axios.get(RANKS_URL)
+        const {data: json} = await axios.get('/stats-data/ranks/latest.json')
         console.log(json)
         this.update = json.update
         this.totalPlayers = json.playerCount
@@ -43,50 +41,176 @@
 
 <template>
   <div id="app">
+    <!-- Title & mini dashboard -->
     <header>
-      <div class="block">
-        <strong><a href="https://craft.moe/">毛玉線圈物語</a>玩家数据榜单<sup>「非官方」</sup></strong>
-        <span>数据新鲜度：{{update}}</span>
+      <div class="wrapper">
+        <div class="title">
+          <strong><a href="https://craft.moe/">毛玉線圈物語</a>玩家数据榜单<sup>「非官方」</sup></strong>
+          <span>数据新鲜度：{{update}}</span>
+        </div>
+        <div class="dashboard">
+          <dl>
+            <div>
+              <dt>玩家总数</dt>
+              <dd>{{totalPlayers}}</dd>
+            </div>
+            <div>
+              <dt>昨日登录</dt>
+              <dd>{{recentlyVisited || 0}}</dd>
+            </div>
+            <div>
+              <dt>昨日入服</dt>
+              <dd>{{newbies.length || 0}}</dd>
+            </div>
+            <div>
+              <dt>黑名单</dt>
+              <dd>123</dd>
+            </div>
+            <div v-if="missingPlayers">
+              <dt>&gt;w&lt;</dt>
+              <dd>{{missingPlayers}}</dd>
+            </div>
+          </dl>
+        </div>
       </div>
     </header>
-    <div class="newbies">
-      <div v-if="newbies.length" class="block">
-        <strong>欢迎萌新</strong><span>昨日入服的玩家（0点之后入服的于次日显示）</span>
-        <ul>
-          <li v-for="playername of newbies"><code>{{playername}}</code></li>
-        </ul>
-        <p>共 {{totalPlayers}} 名有效玩家中，有 {{recentlyVisited}} 名在过去 24 小时内登录过</p>
-      </div>
-      <div v-if="birthdays.length" class="block">
-        <strong>生日快乐</strong><span>N 年前的今天，你们来到了毛线</span>
+
+    <!-- HAPPY BIRTHDAY -->
+    <section v-if="birthdays.length" class="birthdays">
+      <div class="wrapper">
+        <h2>生日快乐</h2>
+        <p>N 年前的今天，你们来到了毛线</p>
         <ul>
           <li v-for="playername of birthdays"><code>{{playername}}</code></li>
         </ul>
       </div>
-    </div>
+    </section>
+
     <section class="ranks">
-      <div style="width: 930px; height: 4px; margin: auto; background: #f0f;"></div>
+      <pre style="width: 1000px;background: #F0F;margin: auto;text-align: center;font-size: 10px;">1000px</pre>
       <div class="wrapper">
         <Rank v-for="(rank, idx) of ranks" :key="idx" :title="rank.title" :items="rank.players" :unit="rank.unit" :description="rank.description"/>
       </div>
     </section>
+
     <footer>
-      <div class="block">
-        <p v-if="missingPlayers">本次有 {{missingPlayers}} 名玩家的数据被吃掉了 &gt;w&lt;</p>
+      <div class="wrapper">
         <p>Made with ❤ by SilentDepth</p>
       </div>
     </footer>
   </div>
 </template>
 
-<style lang="postcss">
+<style>
   @import './styles/global.css';
 
-  section > .wrapper {
-    width: 930px;
+  html, body, #app {
+    margin: 0;
+    height: 100%;
+    font-family: sans-serif;
+    background: #eee;
   }
 
-  .ranks {
+  code {
+    font-family: Consolas, Menlo, monospace;
+  }
+
+  header, section, footer {
+    & > .wrapper {
+      width: var(--pageWidth);
+      margin: 0 auto;
+    }
+  }
+
+  header {
+    background: linear-gradient(to top, #222, #333 50%) #333;
+    color: #ddd;
+    font-size: large;
+
+    @media (--phone) {
+      text-align: center;
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+
+    & .title {
+      float: left;
+      line-height: 1;
+      padding: 1em 0;
+
+      & strong {
+        display: block;
+        margin-bottom: .5em;
+      }
+
+      & a {
+        color: #FFF;
+        text-decoration: none;
+        margin-right: .5em;
+      }
+
+      & sup {
+        line-height: 1;
+        vertical-align: baseline;
+        position: relative;
+        top: -.5em;
+      }
+
+      & span {
+        display: block;
+        color: #CCC;
+        font-size: small;
+
+        @media (--phone) {
+          display: block;
+          float: none;
+          margin-top: 1em;
+        }
+      }
+    }
+
+    & .dashboard {
+      float: right;
+      padding: 1em 0;
+
+      & dl {
+        margin: 0;
+
+        & > * {
+          float: left;
+          padding: 0 1em;
+          border-right: 1px solid #666;
+          text-align: center;
+
+          &:first-child {
+            border-left: 1px solid #666;
+          }
+        }
+
+        & dt {
+          font-size: small;
+          line-height: 1;
+        }
+
+        & dd {
+          font-size: large;
+          line-height: 1;
+          margin: .5em 0 0;
+        }
+      }
+    }
+  }
+
+  section {
+    border-top: 1px solid #F9f9f9;
+    border-bottom: 1px solid #CCC;
+  }
+
+  section.ranks {
     @media (--tablet) {
       padding: 0 10px 10px;
     }
@@ -96,11 +220,17 @@
       justify-content: space-between;
       flex-wrap: wrap;
       margin: auto;
+      padding-bottom: 15px;
 
       & > * {
         flex: 0 0 300px;
         margin-top: 15px;
       }
     }
+  }
+
+  footer {
+    color: #AAA;
+    text-align: center;
   }
 </style>
