a00096e5870cd8387d5483ee26cf770422d82287
SilentDepth <silentdepth@outlook.com>
2018-03-02 18:36:45 +0800

    Init commit

---
+++ src/app.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <nav id="nav">#nav</nav>
+    <main id="main">
+      <div class="wrap">
+        <section id="top-panel">#top-panel</section>
+        <section id="boards">
+          <Board v-for="n of 12" :key="n"/>
+        </section>
+      </div>
+    </main>
+    <footer id="footer">#footer</footer>
+  </div>
+</template>
+
+<script>
+  import Board from './components/board'
+
+  export default {
+    components: {
+      Board,
+    },
+  }
+</script>
+
+<style>
+  html, body {
+    margin: 0;
+    height: 100%;
+  }
+
+  #app {
+    min-height: 100%;
+    font-family: -apple-system, "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Source Han Sans CN", "Source Han Sans SC", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    position: relative;
+    display: flex;
+    flex-flow: column nowrap;
+  }
+
+  #nav {
+    background: #f99;
+    flex: 0;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  #main {
+    flex: 1 1 auto;
+  }
+
+  #main > .wrap {
+    margin: 0 auto;
+    width: 1000px;
+  }
+
+  #top-panel {
+    background: #ff9;
+  }
+
+  #boards {
+    background: #9ff;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .board {
+    flex: 0 0 300px;
+    margin-bottom: 30px;
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    background: #99f;
+    flex: 0;
+  }
+</style>
---
+++ src/components/board.vue
@@ -0,0 +1,30 @@
+<template>
+  <article class="board">
+    <h1>.board</h1>
+    <p class="description">.description</p>
+    <ol>
+      <li v-for="n of 10" :key="n">
+        <code>code</code>
+        <span>
+          <strong>strong</strong>
+          <em>em</em>
+        </span>
+      </li>
+    </ol>
+    <p class="lore">.lore</p>
+  </article>
+</template>
+
+<style>
+  .board {
+    background: #9f9;
+  }
+
+  .board li {
+    text-align: left;
+  }
+
+  .board .lore {
+    font-style: italic;
+  }
+</style>
